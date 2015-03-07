7fc76ddb7a9136c6fdb518829eaf3f292d17fd62
Nevena Djaja <nevena.djaja@gmail.com>
2015-03-07 15:39:06 -0800

    created a test service

--- main.js
+++ main.js
@@ -1,5 +1,18 @@
+// empty brackets for no dependencies
 var myApp = angular.module('testApp',[]);
+
+// create a service
+myApp.factory("Data", function() {
+	return {text: "I am data from a service"};
+});
 
 myApp.controller('TestController', ['$scope', function($scope) {
   $scope.message = 'Hola!';
 }]);
+
+myApp.controller('FirstController', ['$scope', 'Data', function($scope, Data) {
+	$scope.data = Data;
+}]);
+
+myApp.controller('SecondController', function() {
+});
