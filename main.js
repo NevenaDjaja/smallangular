847e471a0afb2a1ee51a3ec4e1a3d8840d8f0b27
Nevena Djaja <nevena.djaja@gmail.com>
2015-03-17 20:19:02 -0700

    created a filter to reverse a string

--- main.js
+++ main.js
@@ -3,8 +3,16 @@ var myApp = angular.module('testApp',[]);
 
 // create a service
 myApp.factory("Data", function() {
-	return {text: "I am data from a service"};
+	return {text: "I am some data from a service"};
 });
+
+// add a filter to your app - Data is DI
+myApp.filter('reverse', function(Data) {
+	return function(text) {
+		return text.split("").reverse().join("") + " " + Data.text;
+	};
+});
+
 
 myApp.controller('TestController', ['$scope', function($scope) {
   $scope.message = 'Hola!';
